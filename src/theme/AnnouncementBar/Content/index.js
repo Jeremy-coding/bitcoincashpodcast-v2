cbcc2a60c0d5baded485acdff0fa1c4ffdab4e19
Jeremy <jeremy@bitcoincashpodcast.com>
2026-04-08 14:41:33 +0200

    Fix social media banner logos.

--- src/theme/AnnouncementBar/Content/index.js
+++ src/theme/AnnouncementBar/Content/index.js
@@ -15,57 +15,58 @@ export default function AnnouncementBarContent() {
   const styles = { height: 60, minHeight: 60, width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }
 
   const spanStyle = { paddingLeft: 5, paddingRight: 5 }
+  const imgStyle = { borderRadius: '50%' }
 
   return <div style={styles}>
     <div>
       <span style={spanStyle}>
         <a href="https://www.youtube.com/channel/UCsrDsJnHFnkMnJhEslofyPQ" target="_blank">
-          <img src={youtubeLogo} height={40} width={40} alt="Youtube link" />
+          <img src={youtubeLogo} height={40} width={40} alt="Youtube link" style={imgStyle} />
         </a>
       </span>
       <span style={spanStyle}>
         <a href="https://twitter.com/TheBCHPodcast" target="_blank">
-          <img src={twitterLogo} height={40} width={40} alt="Twitter link" />
+          <img src={twitterLogo} height={40} width={40} alt="Twitter link" style={imgStyle} />
         </a>
       </span>
       <span style={spanStyle}>
         <a href="https://t.me/thebitcoincashpodcast_discussion" target="_blank">
-          <img src={telegramLogo} height={40} width={40} alt="Telegram link" />
+          <img src={telegramLogo} height={40} width={40} alt="Telegram link" style={imgStyle} />
         </a>
       </span>
       <span style={spanStyle}>
         <a href="https://www.twitch.tv/thebitcoincashpodcast" target="_blank">
-          <img src={twitchLogo} height={40} width={40} alt="Twitch link" />
+          <img src={twitchLogo} height={40} width={40} alt="Twitch link" style={imgStyle} />
         </a>
       </span>
       {/* <span style={spanStyle}>
         <a href="https://www.tiktok.com/@thebchpodcast" target="_blank">
-          <img src={tiktokLogo} height={40} width={40} alt="TikTok link" />
+          <img src={tiktokLogo} height={40} width={40} alt="TikTok link" style={imgStyle} />
         </a>
       </span> */}
       {/* <span style={spanStyle}>
         <a href="https://www.reddit.com/r/BitcoinCashPodcast/" target="_blank">
-          <img src={redditLogo} height={40} width={40} alt="Reddit link" />
+          <img src={redditLogo} height={40} width={40} alt="Reddit link" style={imgStyle} />
         </a>
       </span> */}
       <span style={spanStyle}>
         <a href="https://open.spotify.com/show/01N17Oi7sRZM4vYGiTO6uk" target="_blank">
-          <img src={spotifyLogo} height={40} width={40} alt="Spotify link" />
+          <img src={spotifyLogo} height={40} width={40} alt="Spotify link" style={imgStyle} />
         </a>
       </span>
       <span style={spanStyle}>
         <a href="https://podcasts.apple.com/gb/podcast/the-bitcoin-cash-podcast/id1555355070" target="_blank">
-          <img src={applePodcastsLogo} height={40} width={40} alt="Apple Podcasts link" />
+          <img src={applePodcastsLogo} height={40} width={40} alt="Apple Podcasts link" style={imgStyle} />
         </a>
       </span>
       <span style={spanStyle}>
         <a href="https://podcasts.google.com/feed/aHR0cHM6Ly9tZWRpYS5yc3MuY29tL2JpdGNvaW5jYXNocG9kY2FzdC9mZWVkLnhtbA" target="_blank">
-          <img src={googlePodcastsLogo} height={40} width={40} alt="Google Podcasts link" />
+          <img src={googlePodcastsLogo} height={40} width={40} alt="Google Podcasts link" style={imgStyle} />
         </a>
       </span>
       <span style={spanStyle}>
         <a href="https://rss.com/podcasts/bitcoincashpodcast/" target="_blank">
-          <img src={rssLogo} height={40} width={40} alt="RSS link" />
+          <img src={rssLogo} height={40} width={40} alt="RSS link" style={imgStyle} />
         </a>
       </span>
 
